4f4754723965c6475076561f7a79f00d2a0c1f89
rasyidin <raflirasyidin20@gmail.com>
2021-07-06 19:14:05 +0700

    create base adapter

--- app/src/main/java/com/rasyidin/rannote/ui/adapter/BaseAdapter.kt
+++ app/src/main/java/com/rasyidin/rannote/ui/adapter/BaseAdapter.kt
@@ -1,4 +1,31 @@
 package com.rasyidin.rannote.ui.adapter
 
-abstract class BaseAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter<T>(private val layoutId: Int) :
+    RecyclerView.Adapter<BaseAdapter.BaseViewHolder>() {
+
+    protected var data = ArrayList<T>()
+
+    fun setData(listItem: List<T>) {
+        if (listItem.isNullOrEmpty()) return
+        data.clear()
+        data.addAll(listItem)
+        notifyDataSetChanged()
+    }
+
+    class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return BaseViewHolder(
+            LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
 }
