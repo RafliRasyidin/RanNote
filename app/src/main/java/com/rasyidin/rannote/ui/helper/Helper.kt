8829f8e4d5e7d5b4f8438725d9a3a5562c2a8fc5
rasyidin <raflirasyidin20@gmail.com>
2021-07-28 13:19:27 +0700

    create function to get current date

--- app/src/main/java/com/rasyidin/rannote/ui/helper/Helper.kt
+++ app/src/main/java/com/rasyidin/rannote/ui/helper/Helper.kt
@@ -1,2 +1,11 @@
 package com.rasyidin.rannote.ui.helper
 
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.inject.Singleton
+
+@Singleton
+fun getCurrentDate(): String {
+    return SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+}
+
