5d107679cc2b97aabb9ec4c7ee82f6700c05a2f6
RafliRasyidin <raflirasyidin20@gmail.com>
2021-09-15 20:17:10 +0700

    Create UI of DetailAddTodoFragment

---
+++ app/src/main/java/com/rasyidin/rannote/ui/feature/todo/DetailAddTodoFragment.kt
@@ -0,0 +1,10 @@
+package com.rasyidin.rannote.ui.feature.todo
+
+import com.rasyidin.rannote.databinding.FragmentDetailAddTodoBinding
+import com.rasyidin.rannote.ui.base.BaseFragment
+
+class DetailAddTodoFragment :
+    BaseFragment<FragmentDetailAddTodoBinding>(FragmentDetailAddTodoBinding::inflate) {
+
+
+}
