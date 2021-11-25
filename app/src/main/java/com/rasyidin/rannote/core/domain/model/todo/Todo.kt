a6b9af3145075f465ebb290d81c817cee79cd7b0
RafliRasyidin <raflirasyidin20@gmail.com>
2021-11-25 15:05:43 +0700

    override to string

--- app/src/main/java/com/rasyidin/rannote/core/domain/model/todo/Todo.kt
+++ app/src/main/java/com/rasyidin/rannote/core/domain/model/todo/Todo.kt
@@ -14,16 +14,39 @@ data class Todo(
 sealed class RemindMe {
     object Active : RemindMe()
     object Disable : RemindMe()
+
+    override fun toString(): String {
+        return when (this) {
+            is Active -> "Active"
+            is Disable -> "Disable"
+        }
+    }
 }
 
 sealed class Category {
     object Work : Category()
     object Family : Category()
     object School : Category()
+
+    override fun toString(): String {
+        return when (this) {
+            is Work -> "Work"
+            is Family -> "Family"
+            is School -> "School"
+        }
+    }
 }
 
 sealed class Priority {
     object Low : Priority()
     object Medium : Priority()
     object High : Priority()
+
+    override fun toString(): String {
+        return when (this) {
+            is Low -> "Low"
+            is Medium -> "Medium"
+            is High -> "High"
+        }
+    }
 }
